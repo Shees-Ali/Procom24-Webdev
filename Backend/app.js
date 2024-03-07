9f7135c87047d2e1672db3cdea2bb31d74675bf7
Shees_Ali <sheessyed101@gmail.com>
2024-03-08 03:47:08 +0500

    🚀🚀

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ const authMiddleware = require("./api/middleware/authMiddleware");
 var app = express();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE"
