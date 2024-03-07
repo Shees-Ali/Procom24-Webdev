05226a4b70babdf9dd9f50c9c341c3506729d69d
huzaifa-qureshi <huzaifa9819891@gmail.com>
2024-03-08 01:23:54 +0500

    qr code generation

--- Frontend/src/app/components/payment-form/payment-form.component.ts
+++ Frontend/src/app/components/payment-form/payment-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, Injector, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BasePage } from '../../base/base';
+import QRCode from 'qrcode-generator';
 
 @Component({
   selector: 'app-payment-form',
@@ -11,6 +12,7 @@ export class PaymentFormComponent extends BasePage {
   @Input() isCustomer: boolean | undefined;
   paymentForm!: FormGroup<any>;
   user: any;
+  qrCodeImageUrl!: string;
   constructor(injector: Injector) {
     super(injector);
     this.paymentForm = this.formBuilder.group({
@@ -44,11 +46,50 @@ export class PaymentFormComponent extends BasePage {
     }
 
     const res = await this.network.createOrder(this.paymentForm.value);
-
     console.log(res);
     if (res.data) {
+      //Generate QR Code
+      if(this.isCustomer != true){
+        const qrData = this.formatQRData(this.paymentForm.value);
+        console.log(qrData);
+        this.generateQRCode(qrData);
+      }
       this.utility.presentSuccessAlert(res.message);
-      this.router.navigate(['customer/dashboard/payment']);
+      // this.router.navigate(['customer/dashboard/payment']);
     }
+  }
+
+  formatQRData(formValues: any): string {
+    const formatField = (value: string, type: string): string => {
+      const lengthPrefix = value ? value.length.toString().padStart(2, '0') : '00';
+      return `${type}${lengthPrefix}${value}`;
+    };
+  
+    const username = formatField(formValues.username, '01');
+    const email = formatField(formValues.email, '02');
+    const amount = formatField(formValues.amount.toString(), '03');
+    const accountNumber = `04${formValues.accountNumber}`;
+    const merchantAccountNumber = `05${formValues.merchantAccountNumber}`;
+    const bank = formatField(formValues.paymentPurpose, '06');
+    const paymentPurpose = formatField(formValues.paymentPurpose, '07');
+  
+    return `${username}${email}${amount}${accountNumber}${merchantAccountNumber}${bank}${paymentPurpose}`;
+  }
+
+  generateQRCode(data: string) {
+    const qr = QRCode(0, 'L');
+    qr.addData(data);
+    qr.make();
+    const qrImage = qr.createDataURL(4, 0);
+    this.qrCodeImageUrl = qrImage;
+
+    // Do something with the generated QR code image
+  }
+
+  downloadQRCode() {
+    const link = document.createElement('a');
+    link.download = 'qr-code.png';
+    link.href = this.qrCodeImageUrl;
+    link.click();
   }
 }
--- Frontend/src/app/pages/customer/qr-scan/qr-scan.component.ts
+++ Frontend/src/app/pages/customer/qr-scan/qr-scan.component.ts
@@ -1,12 +1,14 @@
-import { Component, Inject, Injector } from '@angular/core';
+import { Component, Inject, Injector, OnInit } from '@angular/core';
 import { BasePage } from '../../../base/base';
+import QRCode from 'qrcode-generator';
+import jsQR from 'jsQR';
 
 @Component({
   selector: 'app-qr-scan',
   templateUrl: './qr-scan.component.html',
   styleUrl: './qr-scan.component.scss'
 })
-export class QrScanComponent extends BasePage {
+export class QrScanComponent extends BasePage{
   isQRuploaded: boolean = false;
   constructor(injector: Injector){
     super(injector);
@@ -16,10 +18,50 @@ export class QrScanComponent extends BasePage {
     
   }
 
-  onFileSelected($event: any) {
-    if ($event.target.files && $event.target.files.length > 0) {
-      console.log("Image Uploaded");
-      this.isQRuploaded = true;
+  async decodeQRCode(imageData: ImageData): Promise<string | null> {
+    const code = jsQR(imageData.data, imageData.width, imageData.height);
+    if (code) {
+      return code.data;
+    }
+    return null;
+  }
+
+  async onFileSelected(event: any) {
+    const file = event.target.files[0];
+    const image = new Image();
+    image.src = URL.createObjectURL(file);
+    image.onload = async () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = image.width;
+      canvas.height = image.height;
+      const context = canvas.getContext('2d');
+      if (context) {
+        context.drawImage(image, 0, 0, image.width, image.height);
+        const imageData = context.getImageData(0, 0, image.width, image.height);
+        const decodedData = await this.decodeQRCode(imageData);
+        if (decodedData) {
+          const parsedData = this.parseQRData(decodedData);
+          console.log(parsedData);
+          // this.isQRuploaded = true;
+
+        } else {
+          console.error('QR code not found');
+        }
+      }
+    };
+  }
+
+  parseQRData(data: string): any {
+    console.log("Data", data)
+    const parsedValues = [];
+      while (data) {
+        const valueNumber = data.slice(0, 2);
+        const valueSize = parseInt(data.slice(2, 4), 10);
+        const value = data.slice(4, 4 + valueSize);
+        parsedValues.push({ valueNumber, value });
+        data = data.slice(4 + valueSize);
+      }
+      console.log(parsedValues);
+      return parsedValues;
     }
   }
-}
