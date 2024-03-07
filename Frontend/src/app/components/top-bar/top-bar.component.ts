c019237c90a8bf2b61ebb9dbb2f5754f7ab85a47
huzaifa-qureshi <huzaifa9819891@gmail.com>
2024-03-07 21:12:20 +0500

    logout

--- Frontend/src/app/components/top-bar/top-bar.component.ts
+++ Frontend/src/app/components/top-bar/top-bar.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Injector, Input, OnInit, inject } from '@angular/core';
+import { BasePage } from '../../base/base';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'top-bar',
@@ -9,7 +12,15 @@ export class TopBarComponent implements OnInit {
  @Input() isCustomer: boolean | undefined;
   title : String | undefined;
 
+  constructor(private router:Router, private userService:UserService){
+  }
   ngOnInit(): void{
     this.title = (this.isCustomer == true) ? 'Customer Portal' : 'PayHabib';
+  }
+
+  logout(){
+    this.userService.logout();
+    if(this.isCustomer) this.router.navigate(['customer/'])
+    else this.router.navigate(['merchant/'])
   }
 }
